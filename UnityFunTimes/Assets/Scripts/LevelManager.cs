5ef9f0bc7f8e4e9e4dee6b94da5a4c5128a9403f
SolarLunix <solarlunix@gmail.com>
2018-01-24 21:05:33 +0000

    Main Menu and First Level

---
+++ UnityFunTimes/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadNextLevel() {
+		int nextLevelNum = SceneManager.GetActiveScene().buildIndex + 1;
+		SceneManager.LoadScene(nextLevelNum);
+	}//end LoadNextLevel
+
+	public void LoadScene(int scene){
+		SceneManager.LoadScene(scene);
+	}
+}
---
+++ UnityFunTimes/Assets/Scripts/Move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public float mvSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+
+		if(Input.GetKey(KeyCode.W))
+			transform.position += transform.forward * mvSpeed * Time.deltaTime;
+		if(Input.GetKey(KeyCode.S))
+			transform.position -= transform.forward * mvSpeed * Time.deltaTime;
+		if(Input.GetKey(KeyCode.A))
+			transform.position -= transform.right * mvSpeed * Time.deltaTime;
+		if(Input.GetKey(KeyCode.D))
+			transform.position += transform.right * mvSpeed * Time.deltaTime;
+	}
+}
