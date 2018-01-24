4ba00c248b996d0d2c454a0b9be84ed96bf58e07
SolarLunix <solarlunix@gmail.com>
2018-01-24 22:40:29 +0000

    Level Menu works.

--- UnityFunTimes/Assets/Scripts/LevelManager.cs
+++ UnityFunTimes/Assets/Scripts/LevelManager.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class LevelManager : MonoBehaviour {
 
 	public void LoadNextLevel() {
 		int nextLevelNum = SceneManager.GetActiveScene().buildIndex + 1;
 		SceneManager.LoadScene(nextLevelNum);
-	}//end LoadNextLevel
+	}
 
-	public void LoadScene(int scene){
-		SceneManager.LoadScene(scene);
+	public void LoadLevel(int lvl){
+		SceneManager.LoadSceneAsync(lvl);
+	}
+
+	public void LoadLastLevel() {
+		int lastLevelNum = SceneManager.GetActiveScene().buildIndex - 1;
+		SceneManager.LoadSceneAsync(lastLevelNum);
 	}
 }
