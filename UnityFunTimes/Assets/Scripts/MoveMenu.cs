59bdb5ad2bae58dd6b89ff36b2c1f708aea2f6a3
SolarLunix <solarlunix@gmail.com>
2018-02-01 21:57:36 +0000

    Level 3 added
    Created a small menu system

---
+++ UnityFunTimes/Assets/Scripts/MoveMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMenu : MonoBehaviour {
+
+	public float mvSpeed = 100f;
+	public bool move = true;
+	public GameObject menu;
+
+	// Use this for initialization
+	void Start () {
+		menu.SetActive(!move);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(move){
+			Vector3 pos = transform.position;
+
+			if(Input.GetKey(KeyCode.W))
+				transform.position += transform.forward * mvSpeed * Time.deltaTime;
+			if(Input.GetKey(KeyCode.S))
+				transform.position -= transform.forward * mvSpeed * Time.deltaTime;
+			if(Input.GetKey(KeyCode.A))
+				transform.position -= transform.right * mvSpeed * Time.deltaTime;
+			if(Input.GetKey(KeyCode.D))
+				transform.position += transform.right * mvSpeed * Time.deltaTime;	
+		}
+
+		if(Input.GetKey(KeyCode.Escape)){
+			move = !move;
+			pause();
+		}
+
+	}
+
+	void pause(){
+		menu.SetActive(!move);
+	}
+}
